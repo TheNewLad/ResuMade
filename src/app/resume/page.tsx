49a0b0522469901c585d63bd8d5384aad0b21fa9
devkdn <22102599+devkdn@users.noreply.github.com>
2023-06-29 16:37:54 -0500

    Cleanup page

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,29 +1,15 @@
 "use client";
 
 import { getResume } from "@/test-files/resume";
-// import { Basics } from "@/components/resume/Basics";
-import { jsPDF } from "jspdf";
-import { Resume } from "@/types/resume";
-import { createPdf } from "@/utils/createPdf";
-import { BasePDFGenerator } from "@/pdf-generators/BasePDFGenerator";
 import { SimpleGenerator } from "@/pdf-generators";
 
 export default function Page() {
   const resume = getResume();
   const pdf = new SimpleGenerator(resume);
 
-  const doc = createPdf(resume);
-  // doc.save("a4.pdf");
-
   return (
     <div className="mx-auto flex flex-col items-center">
       <h1>Resume</h1>
-      {/*<div*/}
-      {/*  id="resume"*/}
-      {/*  className={"aspect-paper-letter bg-white p-12 text-black"}*/}
-      {/*>*/}
-      {/*  <Basics {...basics} />*/}
-      {/*</div>*/}
       <iframe
         src="/api/generate-resume"
         className="aspect-paper-letter w-1/2"
