5bee467cdc72da33dfb0c39c408924ed4a858a28
TheNewLad <22102599+TheNewLad@users.noreply.github.com>
2023-07-06 19:57:19 -0500

    Add FakerJS to generate test resume data

--- src/test-files/resume.ts
+++ src/test-files/resume.ts
@@ -1,6 +1,39 @@
-import resumeJson from "./resume.json";
-import { ResumeType } from "@/types/resume";
+import { faker } from "@faker-js/faker";
+import { ResumeType, WorkType } from "@/types/resume";
 
 export const getResume = (): ResumeType => {
-  return resumeJson;
+  faker.seed(1);
+  return {
+    basics: {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      url: faker.internet.domainName(),
+      phone: faker.phone.number("(###) ###-####"),
+      summary: faker.lorem.paragraph(5),
+      location: {
+        city: faker.location.city(),
+        region: faker.location.state(),
+      },
+    },
+    work: getWorkExperience(),
+  };
+};
+
+const getWorkExperience = (): WorkType[] => {
+  const workExperience: WorkType[] = [];
+  for (let i = 0; i < 3; i++) {
+    workExperience.push(getWork());
+  }
+  return workExperience;
+};
+
+const getWork = (): WorkType => {
+  return {
+    name: faker.company.name(),
+    position: faker.person.jobTitle(),
+    url: faker.internet.url(),
+    startDate: faker.date.past().toISOString(),
+    endDate: faker.date.past().toISOString(),
+    highlights: [faker.lorem.sentence()],
+  };
 };
